b24366b0be9a7fac830335c63f28260090607daa
Akshaya Prakash <Akshaya.Prakash@unico.com.au>
2021-04-16 14:42:02 +1000

    Css modules

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,10 +1,17 @@
 import React from "react"
 import Layout from "../components/Layout"
+import { page, text } from "../examples/about.module.css"
 const about = () => {
   return (
     <Layout>
-      <div className="about-heading">
+      <div className={page}>
         <h1>About Page</h1>
+        <p>
+          Proident culpa reprehenderit nostrud est id aliquip nostrud cupidatat
+          id ullamco qui ut nisi veniam. Sit excepteur excepteur incididunt
+          commodo adipisicing cillum. Tempor est do consequat ad aliqua aliquip
+          aliquip. Velit fugiat est cupidatat quis occaecat enim ad.
+        </p>
       </div>
     </Layout>
   )
--- src/pages/index.js
+++ src/pages/index.js
@@ -5,10 +5,16 @@ import { page, text } from "../examples/home.module.css"
 export default function Home() {
   return (
     <Layout>
-      <div className="home-heading">
+      <div className={page}>
         <h1>Home Page</h1>
         <h1>Hello World</h1>
         <h1>Hello People</h1>
+        <p className={text}>
+          Anim officia ut laboris voluptate fugiat ea tempor. Adipisicing aliqua
+          voluptate sint irure anim duis duis laborum laborum reprehenderit sint
+          nulla pariatur. Pariatur laboris aliquip consequat Lorem labore duis
+          fugiat anim adipisicing labore nulla reprehenderit.
+        </p>
       </div>
 
       <div>
