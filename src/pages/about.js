0869cfab5ad0ffac13a1a0e3400a0808ebdd4850
Akshaya Prakash <akshayaprakash@192-168-1-104.tpgi.com.au>
2021-04-19 11:20:51 +1000

    Styled_Components

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,20 +1,34 @@
 import React from "react"
 import Layout from "../components/Layout"
-import { page, text } from "../examples/about.module.css"
+// import { page, text } from "../examples/about.module.css"
+import styled from "styled-components"
 const about = () => {
   return (
     <Layout>
-      <div className={page}>
+      <Wrapper>
         <h1>About Page</h1>
-        <p>
+        <h1>Hello World</h1>
+        <p className="text">
           Proident culpa reprehenderit nostrud est id aliquip nostrud cupidatat
           id ullamco qui ut nisi veniam. Sit excepteur excepteur incididunt
           commodo adipisicing cillum. Tempor est do consequat ad aliqua aliquip
           aliquip. Velit fugiat est cupidatat quis occaecat enim ad.
         </p>
-      </div>
+      </Wrapper>
     </Layout>
   )
 }
+
+const Wrapper = styled.section`
+  color: blue;
+
+  h1 {
+    color: rebeccapurple;
+  }
+
+  .text {
+    text-transform: uppercase;
+  }
+`
 
 export default about
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,10 +2,12 @@ import React from "react"
 import { Link } from "gatsby"
 import Layout from "../components/Layout"
 import { page, text } from "../examples/home.module.css"
+import { ExampleButton } from "../examples/button"
 export default function Home() {
   return (
     <Layout>
       <div className={page}>
+        <ExampleButton>click me</ExampleButton>
         <h1>Home Page</h1>
         <h1>Hello World</h1>
         <h1>Hello People</h1>
