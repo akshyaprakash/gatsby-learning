f8eae5676ea0574466bfba7dd1ffff2c164b2aed
Akshaya Prakash <akshayaprakash@192-168-1-104.tpgi.com.au>
2021-04-24 15:08:36 +1000

    Image example

---
+++ src/examples/images.js
@@ -0,0 +1,35 @@
+import React from "react"
+import styled from "styled-components"
+import big from "../assets/images/big.jpg"
+import { StaticImage } from "gatsby-plugin-image"
+const Images = () => {
+  return (
+    <Wrapper>
+      <article>
+        <h4>constrained / default</h4>
+        <StaticImage
+          src="../assets/images/recipe-1.jpeg"
+          alt="food"
+          height={400}
+          placeholder="tracedSVG"
+          layout="constrained"
+          className="example-img"
+          as="section"
+        />
+      </article>
+    </Wrapper>
+  )
+}
+
+const Wrapper = styled.section`
+  width: 70vh;
+  margin: 0 auto;
+  article {
+    border: 2px solid red;
+  }
+  .example-img {
+    border-radius: 1rem;
+  }
+`
+
+export default Images
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,11 @@
 import React from "react"
 import Layout from "../components/Layout"
+import Images from "../examples/images"
 export default function Home() {
   return (
     <Layout>
       <h1>Home Page</h1>
+      <Images />
     </Layout>
   )
 }
